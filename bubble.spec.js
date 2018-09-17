035cd63e692c1ec18be5288129b2e8c04ca04d0f
Steve Warren <steve@interactivenyc.com>
2018-09-17 12:42:51 -0400

    attempting to add spyOn

--- bubble.spec.js
+++ bubble.spec.js
@@ -1,4 +1,10 @@
 describe('Bubble Sort', function(){
+
+  // beforeAll(function () {
+  //   spyOn(window, 'doSort').and.callThrough();
+  // });
+
+  
     it('handles an empty array', function(){
       expect( bubbleSort([]) ).toEqual( [] );
     });
@@ -6,5 +12,8 @@ describe('Bubble Sort', function(){
     it('sorts this array', function() {
         expect( bubbleSort([6, 2, 3, 1, 5, 4]) ).toEqual([1, 2, 3, 4, 5, 6])
     })
+
+    // console.log('doSort was called: ', window.doSort.calls.count());
+    
 
   });
